43879069b4dbbbbf9ed4e8a1b9c29623958af5d0
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-11-19 21:15:59 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import ThemeToggler from "./Theme-Toggler";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <a href="https://flowbite.com" className="flex items-center">
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="mr-3 h-6 sm:h-9"
+                alt="Flowbite Logo"
+              />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                ABC University
+              </span>
+            </a>
+            <div className="flex items-center lg:order-2">
+              <ThemeToggler></ThemeToggler>
+            </div>
+            <div
+              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+              id="mobile-menu-2"
+            ></div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
