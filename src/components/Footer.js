cf350526ff8bb3480adfd9dcb27fa32ca1c7007b
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-11-19 21:16:10 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="rounded-lg shadow dark:bg-gray-800">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center flex justify-center">
+        <span className="text-sm text-gray-500 text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            ABC University
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
