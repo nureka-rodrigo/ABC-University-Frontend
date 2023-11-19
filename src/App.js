591fef59b6c74f3b6655ded02a40b69e40a528f8
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-11-19 21:18:56 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Signin from "./pages/Signin";
+import SidebarHod from "./components/Sidebar-Hod";
+import SidebarLecturer from "./components/Sidebar-Lecturer";
+import SidebarStudent from "./components/Sidebar-Studenet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SidebarHod />;
 }
 
 export default App;
