4701f85983da7e2c889e3c20d5889bb337baeee2
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-11-28 22:07:02 +0530

    Added remember me checkbox

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -90,6 +90,28 @@ export default function Signin() {
                         }
                         onChange={(e) => validatePassword(e)}
                     />
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-start pt-3">
+                        <div className="flex items-center h-5">
+                          <input
+                              id="remember"
+                              name="remember"
+                              aria-describedby="remember"
+                              type="checkbox"
+                              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                              required=""
+                          />
+                        </div>
+                        <div className="ml-3 text-sm">
+                          <label
+                              htmlFor="remember"
+                              className="text-gray-500 dark:text-gray-300"
+                          >
+                            Remember me
+                          </label>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                   <button
                       type="submit"
