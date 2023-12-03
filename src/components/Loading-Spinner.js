38c838b0348a50292cf2ae0447eb652e3024aac8
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-12-03 10:40:39 +0530

    Omptimized styles of Loading-Spinner

--- src/components/Loading-Spinner.js
+++ src/components/Loading-Spinner.js
@@ -3,7 +3,7 @@ import {Spinner} from "flowbite-react";
 
 export default function LoadingSpinner() {
     return (
-        <div role="status" className="flex h-screen items-center justify-center">
+        <div role="status" className="fixed inset-0 start-0 top-0 z-[999] flex h-full w-full items-center justify-center bg-slate-950/60">
             <Spinner className="inline h-12 w-12 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"/>
         </div>
     );
