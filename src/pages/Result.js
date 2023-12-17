706ccf4058c4a64ad2f764c593ee94b899c7f899
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2023-12-17 20:33:43 +0530

    Done Result fetching

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,101 +1,67 @@
 import SidebarStudent from "../components/Sidebar-Student";
 import {Select, Table} from "flowbite-react";
 import Footer from "../components/Footer";
+import axios from "axios";
+import {TokenHeader} from "../data/TokenHeader";
+import {toast} from "react-toastify";
+import {ToastSettings} from "../data/ToastSettings";
+import React, {useCallback, useEffect, useState} from "react";
+import LoadingSpinner from "../components/Loading-Spinner";
 
 export default function Result() {
 
-    const resultTable = [
-        {
-            code: "CST 102-2",
-            title: "Introduction to Computer Science",
-            credits: "2",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "CST 101-2",
-            title: "Fundamentals of Electronics",
-            credits: "2",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "CST 121-3",
-            title: "Structured Programming",
-            credits: "3",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "CST 111-2",
-            title: "Essential Mathematics",
-            credits: "2",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "ESD 121-2",
-            title: "English Language Level-I",
-            credits: "2",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "CST 122-2",
-            title: "Web Programming",
-            credits: "2",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "CST 131-2",
-            title: "Fundamentals of Computer Network",
-            credits: "2",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "ESD 161-1",
-            title: "Tamil Language-I",
-            credits: "1",
-            grade: "A+",
-            type: "C",
-        },
-        {
-            code: "BGE 121-2",
-            title: "Ethics and Law Basics",
-            credits: "1",
-            grade: "A+",
-            type: "C",
-        },
-    ];
+    const [result, setResult] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    let selectedSemester = null;
-
-    function submitSemester(e) {
-        selectedSemester = e.target.value;
-        console.log(selectedSemester);
+    const getSelectedSemester = (e) => {
+        const selectedSemester = e.target.value
+        submitSemester(selectedSemester)
     }
+    const submitSemester = useCallback((selectedSemester) => {
+        setIsLoading(true)
+
+        axios
+            .post(`http://127.0.0.1:8000/api/get_result/`, {"semester": selectedSemester}, {
+                ...TokenHeader
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    setResult(response.data)
+                    setIsLoading(false)
+                }
+            })
+            .catch(() => {
+                toast.error('Error loading data!', {
+                    ...ToastSettings
+                });
+                setIsLoading(false)
+            })
+    }, [setIsLoading])
+
+    useEffect(() => {
+        submitSemester(1)
+    }, [submitSemester])
 
     return (
         <>
             <SidebarStudent/>
+            {isLoading && <LoadingSpinner/>}
             <div className="flex flex-col min-h-screen sm:ml-64 mt-14 bg-gray-100 dark:bg-gray-900">
                 <div className="mt-5 ml-5 mr-5 md:w-36">
-                    <form>
-                        <Select
-                            id="semesters"
-                            onChange={(e) => submitSemester(e)}
-                        >
-                            <option>Semester 1</option>
-                            <option>Semester 2</option>
-                            <option>Semester 3</option>
-                            <option>Semester 4</option>
-                            <option>Semester 6</option>
-                            <option>Semester 7</option>
-                            <option>Semester 8</option>
-                        </Select>
-                    </form>
+                    <Select
+                        id="semesters"
+                        defaultValue={"1"}
+                        onChange={(e) => getSelectedSemester(e)}
+                    >
+                        <option value="1">Semester 1</option>
+                        <option value="2">Semester 2</option>
+                        <option value="3">Semester 3</option>
+                        <option value="4">Semester 4</option>
+                        <option value="5">Semester 5</option>
+                        <option value="6">Semester 6</option>
+                        <option value="7">Semester 7</option>
+                        <option value="8">Semester 8</option>
+                    </Select>
                 </div>
                 <div className="overflow-x-auto p-5">
                     <Table hoverable>
@@ -107,20 +73,28 @@ export default function Result() {
                             <Table.HeadCell>Type of Credits</Table.HeadCell>
                         </Table.Head>
                         <Table.Body className="divide-y">
-                            {resultTable.map((value, i) => {
-                                return (
-                                    <Table.Row
-                                        className="bg-white dark:border-gray-700 dark:bg-gray-800"
-                                        key={i}
-                                    >
-                                        <Table.Cell>{value?.code}</Table.Cell>
-                                        <Table.Cell>{value?.title}</Table.Cell>
-                                        <Table.Cell>{value?.credits}</Table.Cell>
-                                        <Table.Cell>{value?.grade}</Table.Cell>
-                                        <Table.Cell>{value?.type}</Table.Cell>
-                                    </Table.Row>
-                                );
-                            })}
+                            {result.length > 0 ? (
+                                result.map((data, i): React.ReactNode => {
+                                    return (
+                                        <Table.Row
+                                            className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                                            key={i}
+                                        >
+                                            <Table.Cell>{data?.course.code}</Table.Cell>
+                                            <Table.Cell>{data?.course.title}</Table.Cell>
+                                            <Table.Cell>{data?.course.credits}</Table.Cell>
+                                            <Table.Cell>{data?.grade}</Table.Cell>
+                                            <Table.Cell>{data?.course.type}</Table.Cell>
+                                        </Table.Row>
+                                    );
+                                })
+                            ) : (
+                                <Table.Row
+                                    className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                                >
+                                    <Table.Cell colSpan="5">No data found</Table.Cell>
+                                </Table.Row>
+                            )}
                         </Table.Body>
                     </Table>
                 </div>
